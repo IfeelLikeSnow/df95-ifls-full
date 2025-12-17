0e442d604890bf13f127dda9258a8c0e549ab688
IfeelLikeSnow <ifeellikesnow@googlemail.com>
2025-12-17 23:13:37 +0100

    Create IFLS_Diagnostics.lua

---
+++ IFLS_Diagnostics.lua
@@ -0,0 +1,4 @@
+-- DF95/IFLS shim: enables require("IFLS_Diagnostics")
+-- Place this file in: %APPDATA%\REAPER\Scripts\IFLS_Diagnostics.lua
+
+return dofile(reaper.GetResourcePath() .. "/Scripts/IFLS/IFLS/Domain/IFLS_Diagnostics.lua")
