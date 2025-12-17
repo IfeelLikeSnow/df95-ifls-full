2f0f5856c697b179e530277557c54894e0b74004
IfeelLikeSnow <ifeellikesnow@googlemail.com>
2025-12-17 23:13:41 +0100

    Create IFLS_ImGui_Core.lua

---
+++ IFLS_ImGui_Core.lua
@@ -0,0 +1,4 @@
+-- DF95/IFLS shim: enables require("IFLS_ImGui_Core")
+-- Place this file in: %APPDATA%\REAPER\Scripts\IFLS_ImGui_Core.lua
+
+return dofile(reaper.GetResourcePath() .. "/Scripts/IFLS/IFLS/Core/IFLS_ImGui_Core.lua")
